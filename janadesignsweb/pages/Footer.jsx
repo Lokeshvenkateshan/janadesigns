c85291e04359ed2188c2971a597b2e62c97aa219
Lokeshvenkateshan <lokeshvenkatesan44@gmail.com>
2025-07-15 15:18:52 +0530

    contact improved

--- janadesignsweb/pages/Footer.jsx
+++ janadesignsweb/pages/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
+import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn, FaPhone, FaPhoneAlt } from 'react-icons/fa';
 import '../styles/footer.css';
 
 const Footer = () => {
@@ -9,6 +9,8 @@ const Footer = () => {
         <div className="footer-brand">
           <h3>Jana Designs</h3>
           <p>Crafting timeless silk saree designs with passion and precision.</p>
+          <br />
+          <p><strong>Address :</strong> Tamilarukalaga Street, Kondalampatty, Salem-636010</p>
         </div>
 
         <div className="footer-links">
@@ -23,11 +25,12 @@ const Footer = () => {
         </div>
 
         <div className="footer-social">
-          <h4>Follow Us</h4>
+          <h4>Contact</h4>
           <div className="social-icons">
-            <a href="https://facebook.com/janadesigns" target="_blank" rel="noreferrer"><FaFacebookF /></a>
-            <a href="https://instagram.com/janadesigns" target="_blank" rel="noreferrer"><FaInstagram /></a>
-            <a href="https://twitter.com/janadesigns" target="_blank" rel="noreferrer"><FaTwitter /></a>
+            <a href="tel:+919500207092" aria-label="Call Jana Designs" title="Call Jana Designs"><FaPhoneAlt /></a>
+            <a href="https://facebook.com/janadesigns" target="_blank" rel="noreferrer" aria-label="Facebook" title="Facebook"><FaFacebookF /></a>
+            <a href="https://www.instagram.com/jana.j.arts?igsh=MXN6OXM0NDhwdzdrMw==https://instagram.com/janadesigns" target="_blank" rel="noreferrer" aria-label="Instagram" title="Instagram"><FaInstagram /></a>
+            <a href="https://twitter.com/janadesigns" target="_blank" rel="noreferrer" aria-label="X" title="X"><FaTwitter /></a>
             {/* <a href="https://linkedin.com/company/janadesigns" target="_blank" rel="noreferrer"><FaLinkedinIn /></a> */}
           </div>
         </div>
