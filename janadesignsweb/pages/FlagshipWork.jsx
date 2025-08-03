e6fbe8b1bba2d8aace9990ccdc6863f1d2fdc84b
Lokeshvenkateshan <lokeshvenkatesan44@gmail.com>
2025-08-03 16:52:40 +0530

    images

--- janadesignsweb/pages/FlagshipWork.jsx
+++ janadesignsweb/pages/FlagshipWork.jsx
@@ -1,7 +1,7 @@
 import FlagshipWorkModule from './FlagshipWorkModule';
-import elephant from '../images/flagship/deer.bmp';
-import wing from '../images/pngwing.com.png';
-import putta from '../images/putta.png';
+import elephant from '../images/flagship/elephant.bmp';
+import deer from '../images/flagship/deer.bmp';
+import peacock from '../images/flagship/peacockandelephant.bmp';
 import React from 'react'
 
 
@@ -11,15 +11,15 @@ const FlagshipWork = () => {
   {
     image: elephant,
     title: 'Exquisite Silk Saree Collection',
-    description: 'Handwoven silk sarees crafted with intricate patterns and vibrant colors. Perfect for every special occasion, these designs embody tradition and elegance.',
+    description: 'Elephant design crafted with intricate patterns and vibrant colors. Perfect for every special occasion, these designs embody tradition and elegance.',
   },
   {
-    image: wing,
+    image: deer,
     title: 'Elegant Kanchipuram Silks',
-    description: 'Traditional Kanchipuram silk sarees with rich zari borders and vibrant hues, ideal for weddings and festivals.',
+    description: 'Deer in saree with rich look and vibrant hues, ideal for weddings and festivals.',
   },
   {
-    image: putta,
+    image: peacock,
     title: 'Modern Fusion Saree Designs',
     description: 'Innovative designs combining traditional weaving techniques with contemporary styles for the fashion-forward.',
   },
