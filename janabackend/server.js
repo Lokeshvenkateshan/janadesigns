671bf95000a2840e741d078b3f15875a8f7007e3
Lokeshvenkateshan <lokeshvenkatesan44@gmail.com>
2025-08-03 16:34:48 +0530

    Image size increased

--- janabackend/server.js
+++ janabackend/server.js
@@ -14,7 +14,10 @@ const port = process.env.PORT || 5001;
 
 connectDb();
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 
 
 const allowedOrigins = [
